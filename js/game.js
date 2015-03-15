c472750a8d091b7d28fa406d55eaad2f900fd913
Heath Attig <heath.attig@gmail.com>
2015-03-15 06:19:34 -0500

    start screen and mission screen

---
+++ js/game.js
@@ -0,0 +1,12 @@
+$(window).load(function() {
+  game.init();
+});
+  
+var game = {
+  // Start preloading assets
+  init: function(){
+    loader.init();
+    $('.gamelayer').hide();
+    $('#gamestartscreen').show();
+  },
+}
